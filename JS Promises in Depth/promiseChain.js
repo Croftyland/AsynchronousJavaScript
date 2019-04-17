4e5351dd8fcecc525308fbe447fd706f7317047b
Christina Landvytovych <Crofty.land@gmail.com>
2019-04-17 15:48:14 +0300

    create error function

--- JS Promises in Depth/promiseChain.js
+++ JS Promises in Depth/promiseChain.js
@@ -13,7 +13,22 @@ function getFilmTitles(films) {
 output.innerText = "Loading..."
 
 fetch(API_URL + "films")
-.then(response => response.json())
-.then(films => {
-    output.innerText = getFilmTitles(films);
-});
+// .then(response => response.json())
+// .then(films => {
+//         output.innerText = getFilmTitles(films);
+//     })
+    .then(response => {
+        return Promise.reject("invalid JSON")
+            .then(films => {
+            output.innerText = getFilmTitles(films);
+        })
+    })
+    // .then(
+        //onFulfilled
+        // response => {
+        //     console.log(response);
+        // },
+    .catch( error => {
+            console.warn(error);
+            output.innerText = ":(";
+        });
